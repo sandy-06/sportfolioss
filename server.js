824c7f8ee2c125db95a4e03f6a54610397f6f53e
Sandra Sanders <sandy-sanders@msn.com>
2022-05-01 12:50:15 -0700

    file upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-//const fileUpLoad = require(express-fileupLoad);
+const fileUpload = require('express-fileupload');
 const path = require('path');
 
 
@@ -13,6 +13,9 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(fileUpload({
+  useTempFiles:true
+}))
 
 //routes
 app.use('/user', require('./routes/userRoute'));
@@ -20,6 +23,7 @@ app.use('/', require('./routes/projectRoute'));
 app.use('/', require('./routes/aboutRoute'));
 app.use('/', require('./routes/educRoute'));
 app.use('/', require('./routes/experienceRoute'));
+app.use('/', require('./routes/upload'));
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost:27017/sportfolioss',
