c85107071a8e97c7acab27f4793900519d63fa11
Sandra Sanders <sandy-sanders@msn.com>
2022-05-02 17:05:31 -0700

    add skills and certification to front and back

---
+++ client/src/components/adminComponents/AboutAdmin.js
@@ -0,0 +1,20 @@
+import React,{useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+const AboutAdmin = () => {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+export default AboutAdmin
