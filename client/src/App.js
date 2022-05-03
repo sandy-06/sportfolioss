23497d357529aef1f94a96c8feefecb5a115e592
Sandra Sanders <sandy-sanders@msn.com>
2022-05-02 18:17:32 -0700

    add backend for skill and certifications

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,35 @@ import Header from './components/Homepages/Header';
 import Navbar from './components/Homepages/Navbar';
 import About from './components/Homepages/About';
 import Education from './components/Homepages/Eduction';
-import Experience from './'
+import Experience from './components/Homepages/Experience';
+import Projects from './components/Homepages/Projects';
+import Contact from './components/Homepages/Contact';
+import Certificate from './components/Homepages/Certificate';
+import Footer from './components/Homepages/Footer';
+import Login from './components/Homepages/Login';
+import Skill from './components/Homepages/Skill'
+//resume check on download
+// admin components
+import Admin from './components/adminComponents/Admin';
+//edit components
+import EditAbout from './components/editComponents/EditAbout';
+import EditCertificate from './components/editComponents/EditCertificate';
+import EditEducation from './components/editComponents/EditEducation';
+import EditExperience from './components/editComponents/EditExperience';
+import EditProjects from './components/editComponents/EditProjects';
+import EditSkills from './components/editComponents/EditSkills';
+
+import {Route} from 'react-router-dom';
+import {Element} from 'react-scroll';
 
 
 
 
-import './App.css';
 
 function App() {
+
+
+
   return (
     <div className="App">
       dfdd
